e3518f4b0d4e4c96003ee50920441e33ebfd12c3
matulism <166720058+matulism@users.noreply.github.com>
2024-04-18 13:58:43 +0200

    added create user

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from utils.crud import read
-from models.data import users
-
+from utils.crud import read, create_user
+from models1.data import users
 
 if __name__ == '__main__':
 
@@ -9,9 +8,12 @@ if __name__ == '__main__':
             print("Menu")
             print("0.Zakończ program.")
             print("1.Pokaż co u znajomych: ")
+            print("2.Dodaj znajomego: ")
             menu_option:str=input("Wybierz dostępną funkcje z menu: ")
             if menu_option == "0":
                 break
             if menu_option == "1":
                 read(users)
+            if menu_option == "2":
+                create_user(users)
 
--- utils/crud.py
+++ utils/crud.py
@@ -3,3 +3,9 @@ def read(users: list[dict])->None:
     for user in users[1:]:
         print(f'Twój znajomy {user["name"]} opublikował: {user["posts"]}')
 
+def create_user(users: list[dict])->None:
+    name: str = input("Podaj imię użytkownika: ")
+    surname: str = input("Podaj nazwisko użytkownika: ")
+    posts: int = int(input("Podaj liczbę postów: "))
+    user: dict = {"name": name, "surname": surname, "posts": posts}
+    users.append(user)
