18b446028261bf43dd8e4c626a12ef9a914f1d48
matulism <166720058+matulism@users.noreply.github.com>
2024-05-20 14:37:19 +0200

    feat: crud dla obsługi markera

--- notatki2.py
+++ notatki2.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 users: list[dict] = [
     {"name": "Mateusz", "surname": "Matulis", "posts": 9},
     {"name": "Janek", "surname": "Mielec", "posts": 20},
@@ -7,13 +10,16 @@ users: list[dict] = [
     {"name": "Mateusz", "surname": "Matysiak", "posts": 33},
     {"name": "Paweł", "surname": "Paszkowski", "posts": 1},
 ]
-user_name: str = input("Kogo szukasz: ")
-for user in users[1:]:
-        if user["name"] == user_name:
-         new_user_name = input("Wprowadź nowe imię: ")
-         new_user_surname = input("Wprowadź nowe nazwisko: ")
-         new_user_posts = input("Wprowadź nową ilość postów: ")
-         user["surname"] = new_user_surname
-         user["name"] = new_user_name
-         user["posts"] = new_user_posts
-print(users)
+
+
+miasto=input('Podaj nazwę miejscowości: ')
+def get_coords(miasto:str) -> list:
+    adres_url=f'https://pl.wikipedia.org/wiki/{miasto}'
+    response = requests.get(adres_url)
+    response_html=BeautifulSoup(response.text,'html.parser')
+    # print(response_html)
+    latitude=float(response_html.select('.latitude')[1].text.replace(',','.'))
+    longitude=float(response_html.select('.longitude')[1].text.replace(',','.'))
+    print([latitude,longitude])
+    return latitude,longitude
+get_coords(miasto)
